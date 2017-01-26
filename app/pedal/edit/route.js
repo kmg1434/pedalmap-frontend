4db76b0abb12521a3098de325ade083345472117
kmg1434 <kmg1434@gmail.com>
2017-01-26 16:22:01 -0500

    Fixed pedal-edit bug by deleting and re-generating
    * edit template now shows, cannot save an edited pedal!!!

--- app/pedal/edit/route.js
+++ app/pedal/edit/route.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('pedal', params.pedal_id);
+  },
+
+  actions: {
+    savePedal (pedal) {
+      let _this = this;
+      pedal.save().then(function () {
+        _this.transitionTo('pedals');
+      });
+    },
+
+    cancelEditPedal (pedal) {
+      pedal.rollbackAttributes();
+      this.transitionTo('pedals');
+
+    },
+
+    willTransition () {
+      this.get('store').peekAll('pedal').forEach(function (pedal) {
+        if (pedal.get('hasDirtyAttributes')) {
+          pedal.rollbackAttributes();
+        }
+      });
+    },
+  },
 });
